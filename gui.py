045f155fefbf13036260ce9fa5d360a378dd6d96
Avishkar24112003 <kambleavishkarsharad11@gmail.com>
2025-07-02 15:24:32 +0530

    gui.py

--- gui.py
+++ gui.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1upPP_iB2k_4dbk1Wzy8FN4TH86OpG-e5
 """
 
-pip install Streamlit
 
 import streamlit as st
 import pandas as pd
